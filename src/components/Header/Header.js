079fb3841ad654e760b18dd8e57028e63a24b91c
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-09-09 11:29:41 +0800

    header component structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './Header.css'
+import logo from '../../img/logo.png'
 
 const Header = () => {
     return (
         <div className='header'>
             {/* logo */}
+            <img src={logo} alt="logo" className='header__logo'/>
+
             {/* search box */}
+            <div className="header__search">
+                <input type="text" className="header__searchInput"/>
+                {/* search icon */}
+            </div>
+
             {/* nav items and icons */}
+            <div className="header__nav">
+                <div className="header__navItem">
+                    <span className="header__navItemLineOne">Hello Guest</span>
+                    <span className="header__navItemLineTwo">Sign In</span>
+                </div>
+
+                <div className="header__navItem">
+                    <span className="header__navItemLineOne">Returns</span>
+                    <span className="header__navItemLineTwo">& Orders</span>
+                </div>
+
+                <div className="header__navItem">
+                    <span className="header__navItemLineOne">Your</span>
+                    <span className="header__navItemLineTwo">Prime</span>
+                </div>
+            </div>
         </div>
     )
 }
