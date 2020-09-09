2e5f6f6420dd04808eab2b373dd7c4221222a4b5
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-09-09 18:34:21 +0800

    router and page not found component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     // BEM naming convention
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+
+            <Route exact path='/'>
+                <Header />
+                <Home />
+            </Route>
+
+            <Route path='/checkout'>
+                <Header />
+                <h1>Checkout</h1>
+            </Route>
+
+            <Route path='*'>
+                <PageNotFound />
+            </Route>
+        
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
