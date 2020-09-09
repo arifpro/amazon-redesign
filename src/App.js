02ea28a173ca420bcdfc44c8d85fc64985bfe5fd
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-09-09 11:06:00 +0800

    header component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
-      <h1>Amazon</h1>
+      <Header/>
+      {/* Home */}
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            {/* logo */}
+            {/* search box */}
+            {/* nav items and icons */}
+        </div>
+    )
+}
+
+export default Header;
